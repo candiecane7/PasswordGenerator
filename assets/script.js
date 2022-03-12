b755d43909b27d226844b9eafed6d67ff599d0c2
Candice Hall <hall.candice@outlook.com>
2022-03-12 11:49:22 -0500

    added if statements to loop back if selections are not chosen

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,21 @@
 // Assignment code here
 
+
+var selection = [""];
+var charactersNum = "1234567890".split(' ');
+var charactersLow = "qwertyuiopasdfghjklzxcvbnm".split(' ');
+var charactersUp = "QWERTYUIOPASDFGHJKLZXCVBNM".split(' ');
+var charactersSpecial = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split(' ');
+
+
 //when button is clicked, ask questions to get information on what kind of password
 var generatePassword = function () {
   //first question will be length of password
   var passLength = window.prompt("How long do you want your password to be? It must be at least 8 characters long, and no more than 128 characters total.");
+  if(passLength < 8 || passLength > 128) {
+    window.alert("You must choose a password length between 8 and 128. Please try again!");
+    return generatePassword();
+    }
   //second question will be if they want uppercase
   var passUpper = window.confirm("Do you want Uppercase letters in your password?");
   //third question is do they want lowercase
@@ -12,17 +24,10 @@ var generatePassword = function () {
   var passSpecial = window.confirm("Do you want special characters in your password?");
   //fifth question is whether they want numeric characters
   var passNum = window.confirm("Do you want numeric characters in your password?");
-
-  //create an empty array to store different character choices
-  var selection = [""];
-  // var for numbered characters
-  var charactersNum = "1234567890".split(' ');
-  //var for lower case characters
-  var charactersLow = "qwertyuiopasdfghjklzxcvbnm".split(' ');
-  //var for upper case characters
-  var charactersUp = "QWERTYUIOPASDFGHJKLZXCVBNM".split(' ');
-  //var for special characters
-  var charactersSpecial = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split(' ');
+    if(!passLower && !passNum && !passSpecial && !passUpper) {
+      window.alert("You must choose at least one type of character for your password. Please try again!")
+      return generatePassword();
+    }
 
 
   //if statements to push the characters into my selection array if they are chosen via the confirm questions
@@ -42,6 +47,8 @@ var generatePassword = function () {
   // this will separate all the characters from eachother
   selection = selection.join("");
 
+
+
   //for loop that will choose as many characters as the prompt input that was chosen
   var finalPass = []
   for (x = 1; x <= passLength; x++) {
@@ -52,7 +59,6 @@ var generatePassword = function () {
   return finalPass.join("");
 }
 
-// 4 strings with all the different letters/characters that the password can choose from
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
